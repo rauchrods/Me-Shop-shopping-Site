d8fdd0cd434d2551f06daed524a717edf541d604
rauch <rauchrodrigues@gmail.com>
2023-05-03 00:27:58 +0530

    2nd commit

---
+++ profile/myprofile.js
@@ -0,0 +1,124 @@
+if(!localStorage.getItem("currentuser")){
+    alert("you need to signup or login first");
+     setTimeout(()=>{
+      window.location.href  ="../index.html";
+     },1000);
+}
+
+  
+  let saveinfobtn = document.querySelector("#save-info");
+  let logoutbtn = document.querySelector("#logout-btn");
+  let form1 = document.querySelector("#profile1");
+  
+  let fnameinp = document.querySelector("#fname-inp");
+  let lnameinp = document.querySelector("#lname-inp");
+
+  let oldpassinp = document.querySelector("#old-pass-inp");
+  let newpassinp = document.querySelector("#new-pass-inp");
+  let cnfnewpassinp = document.querySelector("#cnf-new-pass-inp");
+  
+   let chanepassbtn = document.querySelector("#change-pass");
+  let errmsg1 = document.querySelector("#err-msg1");
+  let errmsg2 = document.querySelector("#err-msg2");
+  
+
+  let currentuser = JSON.parse(localStorage.getItem("currentuser"));
+  fnameinp.value =currentuser.firstname;
+  lnameinp.value = currentuser.lastname;
+
+
+
+  saveinfobtn.addEventListener("click", ()=>{
+    
+    //  e.preventDefault();
+     console.log("hello");
+
+     let usersarr = JSON.parse(localStorage.getItem("users"));
+     console.log(usersarr);
+
+     for(let i=0; i<usersarr.length; i++){
+        if(usersarr[i].email==currentuser.email){
+          currentuser = {
+            firstname:  fnameinp.value,
+            lastname:  lnameinp.value,
+             email: usersarr[i].email,
+             password: usersarr[i].password
+          }
+
+          usersarr.splice(i, 1, currentuser);
+          break;
+        }
+     }
+
+
+       localStorage.setItem("users", JSON.stringify(usersarr));
+       localStorage.setItem("currentuser", JSON.stringify(currentuser));
+
+  });
+
+
+  chanepassbtn.addEventListener("click", ()=>{
+    
+          if(oldpassinp.value == "" || newpassinp.value == ""||  cnfnewpassinp.value ==""){    
+       errmsg2.innerText  = "Error : All the fields are mandatory";
+       errmsg2.style.color = "#FF4F4F";
+       errmsg2.style.display = "inline-block";
+       return;
+     }
+  
+     if(oldpassinp.value ==  newpassinp.value ){
+      errmsg2.innerText  = "New Password matching with old password make sure it is different";
+      errmsg2.style.color = "#FF4F4F";
+      errmsg2.style.display = "inline-block";
+      return;
+     }
+
+     if(newpassinp.value != cnfnewpassinp.value ){
+      errmsg2.innerText  = "confirm password not matching with new password";
+      errmsg2.style.color = "#FF4F4F";
+      errmsg2.style.display = "inline-block";
+      return;
+     }
+
+
+
+     let usersarr = JSON.parse(localStorage.getItem("users"));
+     console.log(usersarr);
+
+     for(let i=0; i<usersarr.length; i++){
+        if(usersarr[i].email==currentuser.email && usersarr[i].password==oldpassinp.value){
+          currentuser = {
+            firstname:  usersarr[i].firstname,
+            lastname:  usersarr[i].lastname,
+             email: usersarr[i].email,
+             password: newpassinp.value
+          }
+
+          usersarr.splice(i, 1, currentuser);
+
+
+          localStorage.setItem("users", JSON.stringify(usersarr));
+          localStorage.setItem("currentuser", JSON.stringify(currentuser));
+   
+          errmsg2.innerText  = "Password changed successfully";
+          errmsg2.style.color = "#7ECD71";
+          errmsg2.style.display = "inline-block";
+          
+          return;
+        }
+     }
+
+
+     errmsg2.innerText  = "old password not matching with existing password";
+     errmsg2.style.color = "#FF4F4F";
+     errmsg2.style.display = "inline-block";
+      
+  });
+
+
+  logoutbtn.addEventListener("click", ()=>{
+    localStorage.removeItem("currentuser");
+  });
+
+
+
